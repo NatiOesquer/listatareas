c44d4ab111275511135403b5c6fea790e5f1f5f5
NatiOesquer <94532066+NatiOesquer@users.noreply.github.com>
2023-11-29 20:30:48 -0300

    creacion de componente logo (#2)

--- tareas/src/pages/Home.jsx
+++ tareas/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "../App.css"
 import Titulo from '../components/titulo/Titulo'
+import Logo from '../components/logo/Logo'
 
 
 export default function Home() {
   return (
     <div>
       <Titulo/>
+      <Logo />
     </div>
   )
 }
