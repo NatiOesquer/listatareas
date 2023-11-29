3f5ab8792c7b110b710e3c0f8a1920eff0b01ccb
NatiOesquer <nati.oesquer@hotmail.com>
2023-11-29 20:42:05 -0300

    creacion de componente materiales

---
+++ tareas/src/components/materiales/Materiales.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./materiales.css"
+
+export default function Materiales() {
+    return (
+        <div className='contenedor-principal'>
+            <label for='materiales'>Materiales:</label>
+            <textarea name='materiales' className='textarea'> </textarea>
+        </div>
+    )
+}
