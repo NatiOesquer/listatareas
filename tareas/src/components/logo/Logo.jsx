c49dcd461be9a299f314a72e5dc0285aa0e09829
NatiOesquer <nati.oesquer@hotmail.com>
2023-11-29 20:30:16 -0300

    creacion de componente logo

---
+++ tareas/src/components/logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./logo.css"
+
+export default function Logo() {
+  return (
+    <div className='caja-logo'>
+       <img  className='logo' src="assets/logos.png" alt="logo" />
+    </div>
+  )
+}
